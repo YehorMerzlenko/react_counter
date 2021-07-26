905a09c93b887136a81fd77502de35017f68b841
Yehor Merzlenko <yehor.merzlenko@gmail.com>
2021-07-26 12:11:35 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.387d373a2c3a1257ecb04b41d8ba5444.js"
 );
 
 self.addEventListener('message', (event) => {
